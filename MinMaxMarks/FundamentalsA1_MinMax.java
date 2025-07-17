0c48de871b6d16c80f7c0983afd73bd080045ca8
felipessgg <fsepulvedagalvez@gmail.com>
2025-07-17 19:57:35 +1000

    Initializing the project

---
+++ MinMaxMarks/FundamentalsA1_MinMax.java
@@ -0,0 +1,14 @@
+/**
+ * Calculating and printing the highest and lowest mark
+ *
+ * @author Felipe Sepulveda
+ * @version 1
+ */
+import java.util.Scanner;
+
+public class FundamentalsA1_MinMax
+{
+    public static void main(String[] args){
+        
+    }
+}
