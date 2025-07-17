a4aefbbb3d38a056db6f21b3840e596763be5190
felipessgg <fsepulvedagalvez@gmail.com>
2025-07-17 20:13:21 +1000

    Asking for marks and taking min/max

--- MinMaxMarks/FundamentalsA1_MinMax.java
+++ MinMaxMarks/FundamentalsA1_MinMax.java
@@ -17,5 +17,19 @@ public class FundamentalsA1_MinMax
         System.out.print("Enter number of marks: ");
         N = scanner.nextInt();
         double[] student_marks = new double[N];
+        
+        for (int i = 0; i < N; i++){
+            System.out.print("Insert mark number " + (i+1) + ":");
+            student_marks[i] = scanner.nextDouble();
+            
+            if (student_marks[i] < min){ // New min
+                min = student_marks[i];
+            }else if (student_marks[i] > max){
+                max = student_marks[i];
+            }
+        }
+        
+        System.out.println("Max Mark: " + max);
+        System.out.println("Min Mark: " + min);
     }
 }
